fa6c1b6ea3cb722d6655d15fa738f67bbd6b436c
Stefan Friedrich <friedrich@kreisvier.ch>
2018-08-24 11:22:24 +0200

    v1

---
+++ src/translations/en/pluginx.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * pluginx plugin for Craft CMS 3.x
+ *
+ * test plugin craft3
+ *
+ * @link      https://github.com/stefanfriedrich
+ * @copyright Copyright (c) 2018 Stefan Friedrich
+ */
+
+/**
+ * pluginx en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('pluginx', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Stefan Friedrich
+ * @package   Pluginx
+ * @since     1.0.0
+ */
+return [
+    'pluginx plugin loaded' => 'pluginx plugin loaded',
+];
---
+++ src/twigextensions/PluginxTwigExtension.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * pluginx plugin for Craft CMS 3.x
+ *
+ * test plugin craft3
+ *
+ * @link      https://github.com/stefanfriedrich
+ * @copyright Copyright (c) 2018 Stefan Friedrich
+ */
+
+namespace stefanx\pluginx\twigextensions;
+
+use stefanx\pluginx\Pluginx;
+
+use Craft;
+
+/**
+ * Twig can be extended in many ways; you can add extra tags, filters, tests, operators,
+ * global variables, and functions. You can even extend the parser itself with
+ * node visitors.
+ *
+ * http://twig.sensiolabs.org/doc/advanced.html
+ *
+ * @author    Stefan Friedrich
+ * @package   Pluginx
+ * @since     1.0.0
+ */
+class PluginxTwigExtension extends \Twig_Extension
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'Pluginx';
+    }
+
+    /**
+     * Returns an array of Twig filters, used in Twig templates via:
+     *
+     *      {{ 'something' | someFilter }}
+     *
+     * @return array
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('someFilter', [$this, 'someInternalFunction']),
+        ];
+    }
+
+    /**
+     * Returns an array of Twig functions, used in Twig templates via:
+     *
+     *      {% set this = someFunction('something') %}
+     *
+    * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('someFunction', [$this, 'someInternalFunction']),
+        ];
+    }
+
+    /**
+     * Our function called via Twig; it can do anything you want
+     *
+     * @param null $text
+     *
+     * @return string
+     */
+    public function someInternalFunction($text = null)
+    {
+        $result = $text . " in the way";
+
+        return $result;
+    }
+}
